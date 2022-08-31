f601ce47a109f69b394674e2f45b046fb63a9685
VetrovAnton228 <55150813+Anton554@users.noreply.github.com>
2022-08-31 16:17:29 +0300

    Изменен mysite_nginx

--- Businessas/Businessas/wsgi.py
+++ Businessas/Businessas/wsgi.py
@@ -11,9 +11,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 if os.environ.get('DEV') is True:
-   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 else:
-   os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-   "config.settings.production")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "config.settings.production")
 
 application = get_wsgi_application()
