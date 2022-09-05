6c658d93a908e9f617703cd93420204420b5cf8a
VetrovAnton228 <55150813+Anton554@users.noreply.github.com>
2022-09-06 01:16:30 +0300

    Создана сырая версия страницы

--- Businessas/Businessas/urls.py
+++ Businessas/Businessas/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('qr_code', include('qr_code.urls'))
+    path('qr-code', include('qr_code.urls'))
 ]
