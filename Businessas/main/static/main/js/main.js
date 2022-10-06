a166df6bb4593ea17730f4a6f88013e6dd568f52
VetrovAnton228 <55150813+Anton554@users.noreply.github.com>
2022-10-06 23:52:21 +0300

    Добавление анимации движения текста

--- Businessas/main/static/main/js/main.js
+++ Businessas/main/static/main/js/main.js
@@ -128,5 +128,18 @@
     }
   });
 
+  $(function(){
+    $("#typing").typed({
+      strings: [" В Африке гориллы.", " В Африке большие.", " Злые крокодилы."],
+      typeSpeed: 70,
+      backDelay: 1500,
+      startDelay: 2500,
+      loop: true,
+      loopCount: 2,
+      contentType: 'html',
+    });
+
+  });
+
 })(jQuery);
 
